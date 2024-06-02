fc78bceab75b888b41ba8d941e15c5a5edf90567
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-02 13:26:59 -0700

    check interivew code

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     list = []
-    x = 0
     for x in range(n):
         templist = []
-        y = 0
         for y in range(x+1):
             if (y == 0 or y == x):
                 templist.append(1)
             else:
                 templist.append(list[x-1][y-1] + list[x-1][y])
         list.append(templist)
-        # x+=1
     return list
