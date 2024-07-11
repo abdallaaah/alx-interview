1500a0a2dbc7f0d568d7ff12b13bca16f67557d0
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-10 22:33:01 -0700

    check fetch characters from api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,41 @@
 #!/usr/bin/node
-// fetch characters name from movie id, it is request inside request
 
-const { promises } = require('dns')
-const request = require('request')
-let x = process.argv[2]
-url = `https://swapi-api.alx-tools.com/api/films/${x}`
-request(url, (error, response, body) =>{
+const request = require('request');
+let x = process.argv[2];
+let url = `https://swapi-api.alx-tools.com/api/films/${x}`;
+
+// Function to wrap request in a promise
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+request(url, (error, response, body) => {
     if (error) {
-        console.log(error)
+        console.log(error);
+        return;
     }
-    body = JSON.parse(body)
-    list_of_character = body['characters']
-    Promise.all(list_of_character.map(url => request(url, (error, response2, body2)=>{
-        if (error){
-            console.log(error)
-        }
-        x = JSON.parse(body2)
-        console.log(x['name'])
-    })))
-    
-})
+    body = JSON.parse(body);
+    let list_of_character = body['characters'];
+
+    // Map each URL to its index and fetch the character data
+    Promise.all(list_of_character.map((url, index) => 
+        fetch(url).then(character => ({ index, character }))
+    ))
+    .then(results => {
+        // Sort the results based on the original order
+        results.sort((a, b) => a.index - b.index);
+        results.forEach(result => {
+            console.log(result.character.name);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
